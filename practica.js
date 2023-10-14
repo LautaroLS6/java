e88d7ca4e17986504e088571b7bbce171f3fde99
Lautaro <lautarodibujo@gmail.com>
2023-10-13 22:33:09 -0300

    probando

---
+++ practica.js
@@ -0,0 +1,7 @@
+let gordo = "Dante alexis bazan";
+let nombre = prompt ("dime tu nombre")
+    if (nombre == gordo) {
+     alert ("Que onda gordo puto")    
+     } else {
+     alert ("Hola " + nombre)
+     };
